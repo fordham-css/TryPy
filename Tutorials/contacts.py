93c66098aa22bb45f6ccafcb48efabc8da58a432
nickdibari <ndibari@fordham.edu>
2017-03-22 09:11:49 -0400

    Added DB Management

--- Tutorials/contacts.py
+++ Tutorials/contacts.py
@@ -2,23 +2,72 @@
 conatcts.py
 ----------
 Contact Book Application to CRUD personal contacts via a shelve DB
-	- Functions
-	- Objects
-	- Modules
+    - Functions
+    - Objects
+    - Modules
 ----------
 Python Demo Workshop, March 22nd 2017
 '''
 ### Import Modules
 import shelve # Database Management Module [https://docs.python.org/2/library/shelve.html]
+import os
+
+# Database Name
+DB_NAME = '.Contacts.db'
 
 ### Contact Class
 # Python Class definiton of model used to represent personal contacts
 class Contact():
 
-	# Constructor Method
-	def __init__(self, name, phone, address, email, nickname=None):
-		self.name = name
-		self.phone = phone
-		self.address = address
-		self.email = email
-		self.nickname = nickname
+    # Constructor Method
+    def __init__(self, name, phone, address, email, nickname=None):
+        self.name = name
+        self.phone = phone
+        self.address = address
+        self.email = email
+        self.nickname = nickname
+
+# open_DB
+# Returns open instance of shelve object
+def open_DB():
+    try:
+        db = shelve.open(DB_NAME)
+
+        # Syntax to check if db was opened properly
+        # Verbose condition needed, as first time DB is opened it is empty
+        #   -> Checking <if db> would not display confirmation, even though its true
+        if db is not None:
+            print('Opened DB to read: {0}'.format(DB_NAME))
+
+        return db
+
+    # ERROR CASE[1]: Unable to open Database
+    except Exception:
+        print('ERROR[1]: Unable to open Database')
+        print('Exception information: {0}'.format(e))
+
+        exit(1) # !! IF WE GOT HERE WE MESSED UP !! *#
+
+
+# close_DB
+# Closes instance of shelve object
+def close_DB(db):
+    try:
+        db.close()
+        return True
+
+    # ERROR CASE[1]: Unable to close Database
+    except Exception:
+        print('ERROR[2]: Unable to open Database')
+        print('Exception information: {0}'.format(e))
+
+        exit(2) # !! IF WE GOT HERE WE MESSED UP !! *#
+
+# main
+# Driver of program; Calls other functions that do stuff
+def main():
+    pass
+
+# Entry Point of Script
+if __name__ == '__main__':
+    main()
