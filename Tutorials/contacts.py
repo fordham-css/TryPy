9cf2512bcbe915738b88155bbc7c1766354dcb54
nickdibari <ndibari@fordham.edu>
2017-03-22 08:57:19 -0400

    Created Contact Class

--- Tutorials/contacts.py
+++ Tutorials/contacts.py
@@ -10,3 +10,15 @@ Python Demo Workshop, March 22nd 2017
 '''
 ### Import Modules
 import shelve # Database Management Module [https://docs.python.org/2/library/shelve.html]
+
+### Contact Class
+# Python Class definiton of model used to represent personal contacts
+class Contact():
+
+	# Constructor Method
+	def __init__(self, name, phone, address, email, nickname=None):
+		self.name = name
+		self.phone = phone
+		self.address = address
+		self.email = email
+		self.nickname = nickname
