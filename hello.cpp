deb22f204b4401acbf6b27a9fb3df60380af44ab
nickdibari <ndibari@fordham.edu>
2017-03-22 04:53:39 -0400

    Added Hello World in C++

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+/* hello.cpp                             */
+/* Hello World in C++                    */
+/* Python Demo Workshop, March 22nd 2017 */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello world!" <<endl;
+    return 0;
+}
