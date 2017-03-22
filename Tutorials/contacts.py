631733debb4335a6fa3ee5d71156233fb2c7c775
nickdibari <ndibari@fordham.edu>
2017-03-22 08:23:19 -0400

    Added contacts.py

---
+++ Tutorials/contacts.py
@@ -0,0 +1,12 @@
+'''
+conatcts.py
+----------
+Contact Book Application to CRUD personal contacts via a shelve DB
+	- Functions
+	- Objects
+	- Modules
+----------
+Python Demo Workshop, March 22nd 2017
+'''
+### Import Modules
+import shelve # Database Management Module [https://docs.python.org/2/library/shelve.html]
