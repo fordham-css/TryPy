3d9f434c598538ce5fd86090afea9fb656e08ee6
nickdibari <ndibari@fordham.edu>
2017-03-23 23:17:27 -0400

    Switched tabs -> spaces jeeves.py

--- Tutorials/jeeves.py
+++ Tutorials/jeeves.py
@@ -2,8 +2,8 @@
 jeeves.py
 ----------
 Robot to suggest outfit based on the weather
-	- Variables
-	- Conditionals
+    - Variables
+    - Conditionals
 ----------
 Python Demo Workshop, March 22nd 2017
 '''
@@ -38,48 +38,48 @@ condition = raw_input('Now would you say it is clear, cloudy, or overcast?\n > '
 # Error Checking on condition
 condition = condition.lower() # For the purposes of comparison
 if condition != 'clear' and condition != 'cloudy' and condition != 'overcast':
-	print('You are being ridiculous..\nI shall not work with someone with such rude manner!')
+    print('You are being ridiculous..\nI shall not work with someone with such rude manner!')
 
-	exit(1) # !! IF WE GOT HERE WE MESSED UP !! *#
+    exit(1) # !! IF WE GOT HERE WE MESSED UP !! *#
 
 ### Branch to determine what outfit to wear
 
 # Report on current temperature
 if temp > HIGH_THRESHOLD:
-	print('Goodness me it is hot today!')
+    print('Goodness me it is hot today!')
 
 elif temp < LOW_THRESHOLD:
-	print('Sounds like a cold day outside, better bundle up!')
+    print('Sounds like a cold day outside, better bundle up!')
 
 else:
-	print('Seems like a moderate day today')
+    print('Seems like a moderate day today')
 
 # Decide on what to wear based on weather condition
 # Boolean Data Type
 good_weather = True
 
 if condition != 'clear':
-	good_weather = False
+    good_weather = False
 
 if good_weather: # Will execute if good_weather == True
-	if temp >= SHORTS_THRESHOLD:
-		print('Fabulous news! It is nice enough outside to wear shorts! Rock a clean tee and get outside!')
+    if temp >= SHORTS_THRESHOLD:
+        print('Fabulous news! It is nice enough outside to wear shorts! Rock a clean tee and get outside!')
 
-	elif temp <= JACKET_THRESHOLD:
-		print('Better bundle up, if you have to go outside definitely bring a jacket')
+    elif temp <= JACKET_THRESHOLD:
+        print('Better bundle up, if you have to go outside definitely bring a jacket')
 
-	else:
-		print('Your call, remember jeans and a sweater bring it together')
+    else:
+        print('Your call, remember jeans and a sweater bring it together')
 
 else: # Executes on good_weather == False
-	if temp >= SHORTS_THRESHOLD:
-		print('Tough spot...try wearing dri fit shorts with a light rain jacket') 
+    if temp >= SHORTS_THRESHOLD:
+        print('Tough spot...try wearing dri fit shorts with a light rain jacket') 
 
-	elif temp <= JACKET_THRESHOLD:
-		print('Gonna be a tough go. Definitely go with a jacket, hat, and boots if need be')
+    elif temp <= JACKET_THRESHOLD:
+        print('Gonna be a tough go. Definitely go with a jacket, hat, and boots if need be')
 
-	else:
-		print('Try the layered look. Lightweight sweater under a rain jacket')
+    else:
+        print('Try the layered look. Lightweight sweater under a rain jacket')
 
 print('Hope you have a great day {0}!!!'.format(name))
 
