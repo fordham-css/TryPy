8d475fb0fb6e0a9a4ffa2f0d77016b9456fd1eea
nickdibari <ndibari@fordham.edu>
2017-03-23 23:56:09 -0400

    Fixed minor typos and made PEP8 compliant

--- Networking/scrapy.py
+++ Networking/scrapy.py
@@ -6,17 +6,18 @@ website Brainyquote.com
     - Functions
 ----------
 '''
-import requests # For sending requests
-import bs4 # For parsing HTML
+import requests  # For sending requests
+import bs4  # For parsing HTML
 
 # Get the author to search for
 author = raw_input('Author to search for: ')
 
 # Send a request to brainyquote.com with the author formatted in the URL
 # get() method will return a Response object
-# You can do a lot of things with a reponse, like get the JSON of the page,
+# You can do a lot of things with a reponse  like get the JSON of the page,
 # get the HTML of a page, etc...
-response = requests.get('http://brainyquote.com/search_results.html?q={}'.format(author))
+response = requests.get('http://brainyquote.com/search_results.html?q={}'
+                        .format(author))
 
 # Check to ensure page was downloaded correctly
 # Will raise an exception if error occurred (non 200 response code)
@@ -25,20 +26,24 @@ response.raise_for_status()
 
 # Create a BeautifulSoup Object
 # Allows us to parse the HTML retrieved from brainyquote.com
+# You can specify the parser to use, other option include an
+# XML parser
 results = bs4.BeautifulSoup(response.text, 'html.parser')
 
 # Select the quotes
 # Returns a list of Tag objects that are located in a div named "quotesList"
 # and are links (<a> tags)
-# Tag objects are like dictionaries in the way you can retireve values 
+# Tag objects are like dictionaries in the way you can retireve values
 # from the tag's elements, as shown below
-quotes = results.select('#quotesList a') 
+# There are a number of ways to specify what elements of a document to
+# retrieve, consult the BeautifulSoup documentation for more details
+quotes = results.select('#quotesList a')
 
 print('Found the following quotes for {}: \n'.format(author))
 
 for quote in quotes:
-	# Quotes are in tags with <title> == 'view quote'
-	# select() methods will return value of <title> tag
+    # Quotes are in tags with <title> == 'view quote'
+    # select() methods will return value of <title> tag
     if quote.get('title') == 'view quote':
-        print(quote.getText()) # Method to print inner text of tag
+        print(quote.getText())  # Method to print inner text of tag
         print('-')*75
